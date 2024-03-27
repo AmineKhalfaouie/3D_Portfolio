432baf27f63bf8dfe2c9e15ef351a70fe3cdb421
amine <amineemails@gmail.com>
2024-03-27 08:16:13 +0100

    navbar adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import { Home, About, Projects, Contact} from "./pages"
 
 const App = () => {
   return (
@@ -8,10 +9,10 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={'Home'}></Route>
-                    <Route path='/about' element={'About'}></Route>
-                    <Route path='/projects' element={'Projects'}></Route>
-                    <Route path='/contact' element={'Contact'}></Route>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/projects' element={<Projects />}></Route>
+                    <Route path='/contact' element={<Contact />}></Route>
                 </Routes>
             </Router>
         </main>
