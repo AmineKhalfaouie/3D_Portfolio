331d1abc7abc03936679b995b1bcc1048318c202
amine <amineemails@gmail.com>
2024-03-30 12:13:33 +0100

    add island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
 import {Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 {/* <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
         POPUP
       </div> */}
 const Home = () => {
+  const adjustIlandForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9]
+    }
+    else{
+      screenScale = [1, 1, 1]
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIlandForScreenSize()
   return (
     <section className='w-full h-screen relative'>
       <Canvas className='w-full h-screen bg-transparent'
         camera={{near:0.1, far:1000}}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island
+            position = {islandPosition}
+            scale = {islandScale}
+            rotation = {islandRotation}
+          />
+        </Suspense>
       </Canvas>
     </section>
   )
